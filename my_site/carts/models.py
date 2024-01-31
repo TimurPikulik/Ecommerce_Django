e9f03f204265ab99e63062c0abd6eb041fd71b1e
Timur Pikulik <timur.pikulik@gmail.com>
2024-02-01 00:14:41 +0300

    16 added carts logic

--- my_site/carts/models.py
+++ my_site/carts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from users.models import User
+
+from goods.models import Product
+
 
 # Create your models here.
+
+class CartQuerySet(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Пользователь', blank=True, null=True)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    session_key = models.CharField(max_length=32, blank=True, null=True, verbose_name='Ключ сессии')
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        verbose_name = 'Корзины'
+        verbose_name_plural = 'Корзина'
+
+    objects = CartQuerySet().as_manager()
+
+    def __str__(self):
+        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+
+    def products_price(self):
+        return round(self.product.get_discount_price() * self.quantity, 2)
+
+
--- my_site/users/views.py
+++ my_site/users/views.py
@@ -18,7 +18,8 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{user.first_name} выполнил вход")
-                if request.POST.get('next'):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse("user:logout"):
                     return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:
