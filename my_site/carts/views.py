39a371ddd0d6488292395783e72074857d9f1aa9
Timur Pikulik <timur.pikulik@gmail.com>
2024-02-01 23:58:40 +0300

    17 added ajax "add" procedure

--- my_site/carts/views.py
+++ my_site/carts/views.py
@@ -1,13 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from .models import Cart
 from goods.models import Product
+from carts.utils import get_user_cart
 
 
 # Create your views here.
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+    product = Product.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -19,12 +23,26 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META.get('HTTP_REFERER'))
-
+    # return redirect(request.META.get('HTTP_REFERER'))
+    user_cart = get_user_cart(request)
+    cart_items_html = render_to_string(
+        "carts/includes/includedcart.html",
+        context={
+            "carts": user_cart
+        },
+        request=request
+    )
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html
+    }
+    return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
     pass
 
 
-def cart_remove(request, product_slug):
-    pass
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META.get('HTTP_REFERER'))
