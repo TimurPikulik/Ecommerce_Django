23fe1b8025ddb74c36e5eff0ed4965256a6dfbec
Timur Pikulik <timur.pikulik@gmail.com>
2024-02-04 00:04:27 +0300

    20 added order app

--- my_site/users/models.py
+++ my_site/users/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Аватар')
+    phone_number = models.CharField(max_length=20, blank=True, null=True, verbose_name='Номер телефона')
 
     class Meta:
         verbose_name = 'Пользователь'
